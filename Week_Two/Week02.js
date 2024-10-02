6514ec7430f38b7a9925f569643741f8e7210519
FOXAYA <omniasaged111@gmail.com>
2024-10-02 17:25:09 +0300

    week 03

--- Week_Two/Week02.js
+++ Week_Two/Week02.js
@@ -1,35 +1,58 @@
 //Task 01
 
-var NumberOne = "10",
+let NumberOne = "10",
     NumberTwo = "20";
 // Ouput
 console.log(NumberOne + "" + NumberTwo); // Normal Concatenate => 1020
-console.log(NumberOne NumberTwo); // Normal Concatenate => String
-console.log("Add Code Here"); // Template Literals Way => 1020
-console.log("Add Code Here"); // Template Literals Way => String
+console.log(typeof(NumberOne)); // Normal Concatenate => String
+console.log(`${NumberOne}${NumberTwo}`); // Template Literals Way => 1020
+console.log(`${typeof(NumberOne)}`); // Template Literals Way => String
 
-console.log("Add Code Here");
+console.log(NumberTwo + "\n" + NumberOne);
 /*
   Normal Concatenate
   20
   10
 */
-
-console.log("Add Code Here");
+console.log(`${NumberTwo}
+${NumberOne}`);
 /*
   Template Literals Way
   20
   10
 */
-
-
-
-
-
-
-
-
-
-
 console.log("*".repeat(100));
+
+
 //Task 02
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+console.log("*".repeat(100));
+
+
+//Task 03
+console.log('I \'m In'+"\n"+'\\\\'+"\n" + "Love" + "\\\\" + "\"\"\" \'\'\'" +"\n" + "\+\+ with \+\+" + "\n" + "\\\\\"\"\"".repeat(2)+"\n"+"\"\"Javascript\"\"")
+console.log("*".repeat(100));
+
+
+//Task 04
+let a = 21;
+let b = 20;
+let c = b+""+a+"_";
+console.log(`_${21}_${c.repeat(3)}_${b}_`);// _21_2021_2021_2021_20_
+console.log("*".repeat(100));
+
+
+//Challenge>>>>>>>>>>>>>>>>>>>>//
+let userTitle = "Elzero" ,
+    userDesc = "Elzero Web School",
+    userDate = "25 / 10";
+    userInfo = 
+   `<div >
+      <h3>${userTitle}</h3>
+      <p>${userDesc}</p>
+      <span>${userDate}</span>
+      <h1 style= "color:red">${"#".repeat(30)}</h1>
+   </div>
+   `
+   document.write(userInfo.repeat(4));
---
+++ Week_three/Week03.js
@@ -0,0 +1,37 @@
+//Task 01
+console.log((10 * 20 + (15 % 3) + 190 + 10) % 400); // 0
+console.log("*".repeat(100));
+
+
+
+//Task 02
+let num = 3;
+console.log(num + num); // 6
+console.log(num*num - num); // 6
+console.log(num/num + num +(true + true)); // 6
+console.log(num**num / (num) - num); // 6
+console.log(num++ +  true + num++ - (true +true)); // 6
+console.log(++num); // 6
+console.log("*".repeat(100));
+
+
+
+//Task 03
+let num_1 = "10";
+console.log(+num_1 + +num_1); // 20
+console.log(Number(num_1) + Number(num_1)); // 20
+console.log(+num_1 + Number(num_1)); // 20
+console.log(num_1 *= true + true); // 20
+console.log("*".repeat(100));
+
+
+
+//Task 04
+let points = 10;
+let three= true+true+true;
+
+points = points + three;
+console.log(points); // 13
+
+points = points - three - (true+true);
+console.log(points); // 8;
